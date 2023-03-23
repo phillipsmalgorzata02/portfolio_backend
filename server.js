6bfe00f7c59321129d43d2c3b7402ebcd0c1d7ba
Malgorzata Phillips <phillipsmalgorzata02@gmail.com>
2023-03-23 14:09:14 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 8081
+const data = require('./data/resume.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.get('/job/:id', (req, res) => {
+    const job = data.items.filter((f) => f.id == req.params.id)
+    res.send(job)
+})
+
+app.listen(port, () => {
+    console.log(`Server running on ${port}`)
+})
